d30236b2bd1fad5fa6c2a40cba0845d09ad3d410
PetteriM1 <petteri.malkavaara1@gmail.com>
2018-12-16 17:11:22 +0200

    Stuff

--- src/main/java/serversettings/Main.java
+++ src/main/java/serversettings/Main.java
@@ -1,15 +1,14 @@
 package serversettings;
 
-import net.minidev.json.JSONObject;
-import java.io.FileReader;
-import net.minidev.json.parser.JSONParser;
-import cn.nukkit.network.protocol.ServerSettingsResponsePacket;
-import cn.nukkit.network.protocol.ServerSettingsRequestPacket;
-import cn.nukkit.event.server.DataPacketReceiveEvent;
-import cn.nukkit.network.protocol.DataPacket;
-import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerServerSettingsRequestEvent;
+import cn.nukkit.network.protocol.ServerSettingsResponsePacket;
+import cn.nukkit.plugin.PluginBase;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+
+import java.io.FileReader;
 
 public class Main extends PluginBase implements Listener {
 
@@ -20,18 +19,19 @@ public class Main extends PluginBase implements Listener {
     }
 
     @EventHandler
-    public void dataPacket(DataPacketReceiveEvent e) {
-        DataPacket pk = e.getPacket();
-        if (pk instanceof ServerSettingsRequestPacket) {
-            ServerSettingsResponsePacket pk2 = new ServerSettingsResponsePacket();
-            pk2.formId = 5928;
-            try {
-                JSONParser json = new JSONParser();
-                Object obj = json.parse(new FileReader(getDataFolder() + "/settings.json"));
-                JSONObject settings = (JSONObject) obj;
-                pk2.data = settings.toString();
-            } catch(Exception ex) {}
-            e.getPlayer().dataPacket(pk2);
+    public void onSettingsRequest(PlayerServerSettingsRequestEvent e) {
+        ServerSettingsResponsePacket pk = new ServerSettingsResponsePacket();
+        pk.formId = 5928;
+
+        try {
+            JSONParser json = new JSONParser();
+            Object obj = json.parse(new FileReader(getDataFolder() + "/settings.json"));
+            JSONObject settings = (JSONObject) obj;
+            pk.data = settings.toString();
+        } catch(Exception ex) {
+            pk.data = "";
         }
+
+        e.getPlayer().dataPacket(pk);
     }
 }
