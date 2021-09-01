83e5344e9940e104f348623c131d8d314d6cade1
Petteri <26197131+PetteriM1@users.noreply.github.com>
2021-09-01 20:29:08 +0300

    Add a hack to make the menu show up as it seems like Mojang is unable to fix the bug

--- src/main/java/serversettings/Main.java
+++ src/main/java/serversettings/Main.java
@@ -5,33 +5,35 @@ import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerServerSettingsRequestEvent;
 import cn.nukkit.network.protocol.ServerSettingsResponsePacket;
 import cn.nukkit.plugin.PluginBase;
-import net.minidev.json.JSONObject;
-import net.minidev.json.parser.JSONParser;
+import com.nimbusds.jose.shaded.json.JSONObject;
+import com.nimbusds.jose.shaded.json.parser.JSONParser;
 
 import java.io.FileReader;
 
 public class Main extends PluginBase implements Listener {
 
+    private static String DATA;
+
     @Override
     public void onEnable() {
         saveResource("settings.json");
-        getServer().getPluginManager().registerEvents(this, this);
-    }
-
-    @EventHandler
-    public void onSettingsRequest(PlayerServerSettingsRequestEvent e) {
-        ServerSettingsResponsePacket pk = new ServerSettingsResponsePacket();
-        pk.formId = 5928;
-
         try {
             JSONParser json = new JSONParser();
             Object obj = json.parse(new FileReader(getDataFolder() + "/settings.json"));
             JSONObject settings = (JSONObject) obj;
-            pk.data = settings.toString();
+            DATA = settings.toString();
         } catch(Exception ex) {
-            pk.data = "";
+            getLogger().error("Failed to parse settings.json", ex);
+            DATA = "";
         }
+        getServer().getPluginManager().registerEvents(this, this);
+    }
 
-        e.getPlayer().dataPacket(pk);
+    @EventHandler(ignoreCancelled = true)
+    public void onSettingsRequest(PlayerServerSettingsRequestEvent e) {
+        ServerSettingsResponsePacket pk = new ServerSettingsResponsePacket();
+        pk.formId = 5928;
+        pk.data = DATA;
+        getServer().getScheduler().scheduleDelayedTask(this, () -> e.getPlayer().dataPacket(pk), 20);
     }
 }
